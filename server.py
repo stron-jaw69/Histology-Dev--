3dc7c32fd2835c0dff7703eaa7f0117ac458eef5
stron-jaw69 <frk.ganza@gmail.com>
2025-11-23 23:44:39 -0600

    Implement server

---
+++ server.py
@@ -0,0 +1,22 @@
+import socket
+from PIL import Image
+from utils import recv_img
+
+HOST = '127.0.0.1'
+PORT = 40007
+
+def server():
+    p = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    p.bind((HOST, PORT))
+    p.listen(1)
+    print("Server listening on {}:{}".format(HOST, PORT))
+    conn, addr = p.accept()
+    print("Connected by", addr)
+    image = recv_img(conn)
+    image.save('received_result.jpg')
+    print("Image received and saved as received.jpg")
+    conn.close()
+    p.close()
+
+if __name__ == "__main__":
+    server()
